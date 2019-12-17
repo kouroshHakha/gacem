adb2e4003b01093d270d152b0536c79dc06a6015
Kourosh Hakhamaneshi <kourosh_hakhamaneshi@berkeley.edu>
2019-12-17 06:18:45 -0800

    removed torch.tensor(const), used broadcasting instead

--- src/optnet/torch/dist.py
+++ src/optnet/torch/dist.py
@@ -2,17 +2,17 @@ import torch
 
 def cdf_normal(x: torch.Tensor, mean: torch.Tensor, sigma: torch.Tensor):
     x_centered = x - mean
-    erf_x = (x_centered / torch.tensor(2 ** 0.5) / sigma).erf()
+    erf_x = (x_centered / (2 ** 0.5) / sigma).erf()
     return 0.5 * (1 + erf_x)
 
 def cdf_logistic(x: torch.Tensor, mean: torch.Tensor, sigma: torch.Tensor):
     x_centered = x - mean
-    sigmoid_x = (x_centered / torch.tensor(2 ** 0.5) / sigma).sigmoid()
+    sigmoid_x = (x_centered / (2 ** 0.5) / sigma).sigmoid()
     return sigmoid_x
 
 def cdf_uniform(x: torch.Tensor, a: torch.Tensor, b: torch.Tensor):
-    eps = torch.tensor(1e-15)
+    eps = 1e-15
     l = (x - a) / (b - a + eps)
     bot = l.relu()
-    cdf = - (torch.tensor(1)-bot).relu() + 1
+    cdf = - (1.0 - bot).relu() + 1
     return cdf
