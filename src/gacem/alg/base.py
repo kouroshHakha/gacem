d0fceafeb7e137bede27eeae89aeabdb2416e8cd
Kourosh Hakhamaneshi <kourosh_hakhamaneshi@berkeley.edu>
2020-02-22 22:03:43 -0800

    refactored cem: implemented AlgBase

--- src/gacem/alg/base.py
+++ src/gacem/alg/base.py
@@ -1,22 +1,49 @@
 """
 This module implements a base structure for the algorithm with specific methods to be overridden
 """
-from typing import Optional, Mapping, Any
+from typing import Optional, Mapping, Any, Sequence, Dict, Tuple
 
 import time
 import abc
 from pathlib import Path
+import numpy as np
 
 
-from utils.file import read_yaml, write_yaml, get_full_name
+from utils.file import read_yaml, write_yaml, get_full_name, read_pickle, write_pickle
 from utils.loggingBase import LoggingBase
+from utils.data.database import Database
+
+from bb_eval_engine.util.importlib import import_bb_env
+from bb_eval_engine.data.design import Design
+from bb_eval_engine.base import EvaluationEngineBase
+
+from ..viz.plot import plt_hist2D
 
 
 class AlgBase(LoggingBase, abc.ABC):
-    # noinspection PyUnusedLocal
     def __init__(self, spec_file: str = '', spec_dict: Optional[Mapping[str, Any]] = None,
                  load: bool = False, **kwargs) -> None:
-        LoggingBase.__init__(self)
+        """
+        Parameters
+        ----------
+        spec_file: str
+        spec_dict: Dict[str, Any]
+        some non-obvious fields
+            elite_criteria: str
+                'optim': from sorted x1, ..., xn choose p-quantile
+                'csp': constraint satisfaction is enough, from x1, ..., xn
+                choose p-quantile if it is worst than the constraint else choose all which are
+                better than the constraint
+            allow_repeated: bool
+                True to allow repeated samples to be added to the buffer, else all samples in buffer
+                will have equal likelihood when drawn from it.
+            on_policy: bool
+                True to allow on_policy sample usage, meaning that we won't use samples from
+                previous policies to train the current policy (samples are not drawn from
+                CacheBuffer)
+        load: bool
+        kwargs: Dict[str, Any]
+        """
 
         if spec_file:
             specs = read_yaml(spec_file)
@@ -36,6 +63,165 @@ class AlgBase(LoggingBase, abc.ABC):
             self.work_dir = Path(specs['root_dir']) / f'{unique_name}'
             write_yaml(self.work_dir / 'params.yaml', specs, mkdir=True)
 
+        params = specs['params']
+        # create evaluation core instance
+        self.bb_env: EvaluationEngineBase = import_bb_env(params['bb_env'])
+        check_params = params['check_params']
+        self.check_ntimes = check_params['ntimes']
+        self.check_nsamples = check_params['nsamples']
+
+        self.seed = params['seed']
+        self.nsamples = params['nsamples']
+
+        self.db = Database(Design)
+
+        self.state = None
+
+        LoggingBase.__init__(self, self.work_dir)
+
     @abc.abstractmethod
+    def collect_samples(self, n: int, is_random: bool = False, **kwargs) -> Sequence[Design]:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def train(self, iter_cnt: int, samples: Sequence[Design], **kwargs):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def update_state(self, iter_cnt: int):
+        raise NotImplementedError
+
+    @classmethod
+    @abc.abstractmethod
+    def _set_seed(cls, seed):
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def stop(self, iter_cnt) -> bool:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def setup_state(self):
+        """Sets the initial self.state by loading the checkpoint if self.load is True.
+        The algorithm should be able to resume by invoking this function. After calling this
+        function self.state should contain iter_cnt field"""
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def report_accuracy(self, ntimes: int, nsamples: int) -> Tuple[float, float]:
+        """ Reports the accuracy and std of it.
+        Parameters
+        ----------
+        ntimes: int
+            Number of times to do the experiment.
+        nsamples: int
+            Number of samples to draw in each experiment.
+
+        Returns
+        -------
+        acc, std: Tuple[float, float]
+            The average and standard deviation of the accuracy across experiments.
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def sample_model(self, n: int) -> Sequence[Design]:
+        raise NotImplementedError
+
+
+    @abc.abstractmethod
+    def end(self) -> None:
+        """The end goes here! things like plotting .etc"""
+        raise NotImplementedError
+
+    def _run_alg(self):
+        """override this if you want a more custom implementation"""
+        self.setup_state()
+        iter_cnt = self.state['iter_cnt']
+        self.db = self.state['db']
+        while not self.stop(iter_cnt):
+            samples: Sequence[Design] = self.collect_samples(self.nsamples,
+                                                             is_random=(iter_cnt == 0))
+
+            if self.bb_env.ndim == 2:
+                ranges = np.array(list(zip(self.bb_env.params_min, self.bb_env.params_max)))
+                plt_hist2D(np.array(samples), fpath=self.work_dir / f'samples_{iter_cnt}.png',
+                           cmap='binary', range=ranges , show_colorbar=True)
+            avg_obj = np.mean([sample['obj'] for sample in samples])
+            self.info(f'Iter {iter_cnt}: avg_obj = {avg_obj}')
+            self.train(iter_cnt, samples)
+            iter_cnt += 1
+            self.update_state(iter_cnt)
+
+
+    def update_obj(self, designs: Sequence[Design]) -> Sequence[Design]:
+        """ Takes in a list of designs and updates their obj attribute to reflect the objective of
+        cem based algorithms"""
+        def err(val, target, plus=True):
+            if plus:
+                return (val - target) / (val + target + 1e-15)
+            return (target - val) / (val + target + 1e-15)
+        for dsn in designs:
+            objectives = []
+            for key, spec in self.bb_env.spec_range.items():
+                if (spec.ub is None) == (spec.lb is None):
+                    raise ValueError(f'spec {key} has an error in the environment!')
+                elif spec.ub:
+                    # spec has an upper bound
+                    spec_obj = - spec.weight * err(dsn[key], spec.ub)
+                else:
+                    # spec has a lower bound
+                    spec_obj = - spec.weight * err(dsn[key], spec.lb, False)
+                objectives.append(spec_obj)
+            dsn['obj'] = max(objectives)
+
+        return designs
+
+    def save_checkpoint(self, data_dict: Dict[str, Any]) -> None:
+        write_pickle(self.work_dir / 'checkpoint.pickle', data_dict)
+
+    def load_checkpoint(self) -> Dict[str, Any]:
+        return read_pickle(self.work_dir / 'checkpoint.pickle')
+
+    def check_solutions(self, ntimes=1, nsamples=1000):
+        self.log_text('-------- REPORT --------')
+        acc, std = self.report_accuracy(ntimes, nsamples)
+
+        saved_data = dict(acc=acc, std=std)
+        write_yaml(self.work_dir / 'performance.yaml', saved_data)
+
+    def load_and_sample(self, nsamples: int, only_positive: bool = False) -> Sequence[Design]:
+        """sets up the model and generates samples"""
+        self.setup_state()
+        samples = self.sample_model(nsamples)
+
+        if not only_positive:
+            return samples
+
+        n_remaining = nsamples
+        samples = []
+        tried = {}
+        while n_remaining > 0:
+            new_samples = self.sample_model(n_remaining)
+            for sample in new_samples:
+                if sample not in self.db and sample not in tried:
+                    sample,  = self.bb_env.evaluate([sample])
+                    sample, = self.update_obj([sample])
+                elif sample in self.db:
+                    dt = self.db[sample]
+                    sample = self.db[dt]
+                else:
+                    sample = tried[sample]
+
+                if sample['valid'] and sample['obj'] < 0:
+                    tried[sample] = sample
+                    samples.append(sample)
+                    n_remaining -= 1
+
+        return samples
+
     def main(self) -> None:
-        pass
+        self._set_seed(self.seed)
+        self._run_alg()
+        self.check_solutions(ntimes=self.check_ntimes, nsamples=self.check_nsamples)
+        self.end()
