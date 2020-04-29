82f1c6339b240b76f9b80c6a19aaa333f2027689
Kourosh Hakhamaneshi <kourosh_hakhamaneshi@berkeley.edu>
2020-04-29 16:13:25 -0700

    fixed indexing bug in animate.py

--- src/gacem/viz/animate.py
+++ src/gacem/viz/animate.py
@@ -15,7 +15,7 @@ def convert(folder: Union[str, Path], pattern: str, file_name: str, fps: int = 1
     for filename in folder_path.glob(pattern):
         named_images.append(str(filename.absolute()))
 
-    named_images = sorted(named_images, key=lambda x: int(Path(x).stem.split('_')[2]))
+    named_images = sorted(named_images, key=lambda x: int(Path(x).stem.split('_')[-1]))
     clip = mpy.ImageSequenceClip(named_images, fps=fps)
     clip.write_gif(folder_path / f'{file_name}.gif', fps=fps)
 
